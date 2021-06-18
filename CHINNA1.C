9b2c757856573ff21cc0eecc92751747cee4e9bd
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-06-18 09:32:09 +0530

    Add files via upload

---
+++ CHINNA1.C
@@ -0,0 +1,21 @@
+//find the sizes od all data types
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int a;
+ float b;
+ char c;
+ double d;
+ long double e;
+ clrscr();
+
+ printf("size of a is %d\n",sizeof (a));
+ printf("size of b is %d\n",sizeof (b));
+ printf("size of c is %d\n",sizeof (c));
+ printf("size of d is %d\n",sizeof (d));
+ printf("size of e is %d\n",sizeof (e));
+}
+
+
